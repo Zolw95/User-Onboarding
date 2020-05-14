7e6ff92660a5f3ec4c282bfba1b58e7523a02d31
unknown <miloszz87@gmail.com>
2020-05-13 21:18:17 -0400

    includes cypress project

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,23 +1,47 @@
 /* global cy */
 // KEYWORDS: describe, it, context, specify, beforeEach
-
+// ARRANGE - Get the element
+// ACT - MIMIC USER INTERACTION
+// ASSERT - SIMULATE ELEMENT
 // DESCRIBE(Context) - Describes what this file is for // What we are testing in this file
 
 describe("Test our form inputs", function() {
+
     beforeEach(function() {
         // Check if visit is successful
         cy.visit("http://localhost:3000/");
     });
     it("add text to name input", function() {
-        // Test Name Value
-    cy.get('[data-cy="name"]').type("Milosz").should("have.value", "Milosz");
+    // Test Name Value
+    cy.get('[data-cy="name"]')
+    .type("Milosz")
+    .should("have.value", "Milosz")
+    .type("Name is required.")
+    .should("have.length", 1);
     cy.get('[data-cy="email"]')
     .type("email@email.com")
-    .should("have.value", "email@email.com");
-    cy.get('[data-cy="password"]').should("have.value");
-    cy.get('[type="checkbox"]').check().should("be.checked");
+    .should("have.value", "email@email.com")
+    .type("Email is required")
+    .should("have.length", 1);
+    cy.get('[data-cy="password"]')
+    .type("12345678")
+    .should("have.value", "12345678")
+    .type("Password requires minimum 8 characters.")
+    .should("have.length", 8);
+    cy.get('[type="checkbox"]').check().should("be.checked"); // Checks if password has value - if not - throw error
     //cy.contains('Submit').click();
     cy.get("form").submit() 
+
+    // Validate Form
+    // it('check validation message on invalid input', () => {
+    //     cy.get('input:invalid').should('have.length', 0)
+    //     cy.get('[type="email"]').type('not_an_email')
+    //     cy.get('[type="submit"]').click()
+    //     cy.get('input:invalid').should('have.length', 1)
+    //     cy.get('[type="email"]').then(($input) => {
+    //       expect($input[0].validationMessage).to.eq('I expect an email!')
+    //     })
+    //   })
     });
 
     // it("second it test", function() {
